91bbb494f775fdd50aa80a632130ac16258375e4
rhuanpk <rhuan.patriky@rhuanpk.net>
2024-04-03 22:00:43 -0300

    update: golang example

--- programming/golang/vanilla/scripts/regexp_find.go
+++ programming/golang/vanilla/scripts/regexp_find.go
@@ -5,14 +5,19 @@ import (
 	"regexp"
 )
 
-// go playground: https://go.dev/play/p/9c5KCLHwVCw
+// go playground: https://go.dev/play/p/faq8g5aECLp
 func main() {
-	fmt.Println("FindAllString ................:", regexp.MustCompile(`(/[^/]+)`).FindAllString("/foo/bar", -1))
-	fmt.Println("FindAllStringIndex ...........:", regexp.MustCompile(`(/[^/]+)`).FindAllStringIndex("/foo/bar", -1))
-	fmt.Println("FindAllStringSubmatch ........:", regexp.MustCompile(`(/[^/]+)`).FindAllStringSubmatch("/foo/bar", -1))
-	fmt.Println("FindAllStringSubmatchIndex ...:", regexp.MustCompile(`(/[^/]+)`).FindAllStringSubmatchIndex("/foo/bar", -1))
-	fmt.Println("FindString ...................:", regexp.MustCompile(`(/[^/]+)`).FindString("/foo/bar"))
-	fmt.Println("FindStringIndex ..............:", regexp.MustCompile(`(/[^/]+)`).FindStringIndex("/foo/bar"))
-	fmt.Println("FindStringSubmatch ...........:", regexp.MustCompile(`(/[^/]+)`).FindStringSubmatch("/foo/bar"))
-	fmt.Println("FindStringSubmatchIndex ......:", regexp.MustCompile(`(/[^/]+)`).FindStringSubmatchIndex("/foo/bar"))
+	const (
+		regex = `/[^/]+`
+		str   = "/foo/bar"
+	)
+	println("REGEX:", regex, "\nSTRING:", str)
+	fmt.Printf("FindAllString ................: %#v\n", regexp.MustCompile(regex).FindAllString(str, -1))
+	fmt.Printf("FindAllStringIndex ...........: %#v\n", regexp.MustCompile(regex).FindAllStringIndex(str, -1))
+	fmt.Printf("FindAllStringSubmatch ........: %#v\n", regexp.MustCompile(regex).FindAllStringSubmatch(str, -1))
+	fmt.Printf("FindAllStringSubmatchIndex ...: %#v\n", regexp.MustCompile(regex).FindAllStringSubmatchIndex(str, -1))
+	fmt.Printf("FindString ...................: %#v\n", regexp.MustCompile(regex).FindString(str))
+	fmt.Printf("FindStringIndex ..............: %#v\n", regexp.MustCompile(regex).FindStringIndex(str))
+	fmt.Printf("FindStringSubmatch ...........: %#v\n", regexp.MustCompile(regex).FindStringSubmatch(str))
+	fmt.Printf("FindStringSubmatchIndex ......: %#v\n", regexp.MustCompile(regex).FindStringSubmatchIndex(str))
 }
