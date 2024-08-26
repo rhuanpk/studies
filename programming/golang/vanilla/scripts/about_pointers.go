1dd2d356b54eb9f8a5ff68f269aa248d57e88ae8
rhuanpk <dev@rhuanpk.net>
2024-08-26 07:50:19 -0300

    feat: add new examples in about golang pointers

--- programming/golang/vanilla/scripts/about_pointers.go
+++ programming/golang/vanilla/scripts/about_pointers.go
@@ -6,14 +6,14 @@ import "fmt"
 // memória e não com o valor diretamente, ou seja, estamos manipulando o
 // endereço de um espaço na memória que está reservado para guardar algum valor.
 
-// go playground: https://go.dev/play/p/TD77XmpcxxA
+// go playground: https://go.dev/play/p/8eLOPap_D-S
 func main() {
 	println("-----> first example <-----")
 	// 1. declaramos `foo` como um tipo ponteiro de string, ou seja, `foo`
 	// poderá receber apenas endereços para espaços de memória destinados a
 	// guardar valores do tipo string
 	var foo *string
-	fmt.Printf("\n1 step -> var foo *string\n\t- foo (ptr): %v; *foo (val): <panic> [segfault]\n", foo)
+	fmt.Printf("\n1 step -> var foo *string\n\t- foo (val): %v; *foo (drf): <segfault>\n", foo)
 
 	// 2. reservamos (declaramos) um espaço na memória (variável) chamado `aux`
 	// destinado a guardar valores do tipo string ao mesmo tempo que atribuímos
@@ -25,23 +25,45 @@ func main() {
 	// , agora o ponteiro `foo` aponta para o mesmo endereço de memória da
 	// variável `aux` (&aux)
 	foo = &aux
-	fmt.Printf("\n3 step -> foo = &aux\n\t- foo (ptr): %v; *foo (val): %#v\n\t- &aux (ptr): %v; aux (val): %#v\n", foo, *foo, &aux, aux)
+	fmt.Printf("\n3 step -> foo = &aux\n\t- foo (val): %v; *foo (drf): %#v\n\t- &aux (ptr): %v; aux (val): %#v\n", foo, *foo, &aux, aux)
 
 	// 4. agora, independendo se alterarmos o espeço na memória de `*foo` quanto
 	// de `aux`, o valor se refletirá em ambos pois, ambos (`foo` e `&bar`)
 	// apontam para o mesmo endereço na memória
 	aux = "world"
-	fmt.Printf("\n4 step -> aux = \"world\"\n\t- foo (ptr): %v; *foo (val): %#v\n\t- &aux (ptr): %v; aux (val): %#v\n", foo, *foo, &aux, aux)
+	fmt.Printf("\n4 step -> aux = \"world\"\n\t- foo (val): %v; *foo (drf): %#v\n\t- &aux (ptr): %v; aux (val): %#v\n", foo, *foo, &aux, aux)
 	*foo = "xpto"
-	fmt.Printf("\n4 step -> *foo = \"xpto\"\n\t- foo (ptr): %v; *foo (val): %#v\n\t- &aux (ptr): %v; aux (val): %#v\n", foo, *foo, &aux, aux)
+	fmt.Printf("\n4 step -> *foo = \"xpto\"\n\t- foo (val): %v; *foo (drf): %#v\n\t- &aux (ptr): %v; aux (val): %#v\n", foo, *foo, &aux, aux)
 
 	println("\n-----> second example <-----")
 	// 1. `new()` irá alocar espaço na memória com o valor zero do tipo passado
 	// e retornar o endereço desse espaço alocado
 	var bar *string = new(string)
-	fmt.Printf("\n1 step -> var bar *string = new(string)\n\t- bar (ptr): %v; *bar (val): %#v\n", bar, *bar)
+	fmt.Printf("\n1 step -> var bar *string = new(string)\n\t- bar (val): %v; *bar (drf): %#v\n", bar, *bar)
 
 	// 2. atribuímos o valor "xpto" no espaço da memória do ponteiro `bar`
 	*bar = "xpto"
-	fmt.Printf("\n2 step -> *bar = \"xpto\"\n\t- bar (ptr): %v; *bar (val): %#v\n", bar, *bar)
+	fmt.Printf("\n2 step -> *bar = \"xpto\"\n\t- bar (val): %v; *bar (drf): %#v\n", bar, *bar)
+
+	println("\n-----> third example <-----")
+	// 1. não devemos esquecer também que variáveis do tipo ponteiro são
+	// variáveis como quaisquer outras, ou seja, quando criamos uma variável do
+	// tipo ponteiro, simplesmente estamos reservando um espaço na memória para
+	// guardar valores de endereço de memória (de outros espaços na memória, que
+	// por sua vez, estão reservados para alocar valor de algum tipo de dado),
+	// ou seja, como variáveis do tipo ponteiros também são variáveis "comuns",
+	// elas também tem seu próprio endereço na memória
+	var boo *any
+	fmt.Printf("\n1 step -> var boo *any\n\t- &boo (ptr): %v; boo (val): %#v; *boo (drf): <segfault>\n", &boo, boo)
+
+	// 2. se tentamos atribuir valores diretamente a desreferência de ponteiros
+	// sem antes o inicializarmos, pegamos um erro de "invalid memory address or
+	// nil pointer dereference" (segfault) pois, se ainda não o inicializamos,
+	// o valor do ponteiro (que é um endereço de memória) está nil, ou seja,
+	// como não há valor algum, não está apontando para lugar nenhum na memória,
+	// então quando desreferênciamos o ponteiro (acessamos o espaço na memória
+	// no qual o ponteiro aponta) ele não existe, então como podemos atribuir
+	// valor num espaço na memória que não existe?
+	var baz *any = new(any)
+	fmt.Printf("\n2 step -> var baz *any = new(any)\n\t- &baz (ptr): %v; baz (val): %#v; *baz (drf): %#v\n", &baz, baz, *baz)
 }
