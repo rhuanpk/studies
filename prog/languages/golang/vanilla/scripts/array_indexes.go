f517d2b645054b439493726152b7a8575834b738
rhuanpk <dev@rhuanpk.net>
2025-05-21 13:58:04 -0300

    feat: add obs in go example

--- prog/languages/golang/vanilla/scripts/array_indexes.go
+++ prog/languages/golang/vanilla/scripts/array_indexes.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-// go playground: https://go.dev/play/p/k2FwEIAqDM0
+// go playground: https://go.dev/play/p/dSoVtbivadx
 func main() {
 	array := [...]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
 	fmt.Printf("array to example: %#v\n\n", array)
@@ -27,4 +27,6 @@ func main() {
 
 	println("\nappend(array[:4], array[6:]...) -> cutting indexes 4 and 5:")
 	fmt.Println(append(array[:4], array[6:]...))
+
+	fmt.Println("\nsyntax is 'array[<start>:<end>]' since indexes are 'inclusive' and 'exclusive', respectively")
 }
