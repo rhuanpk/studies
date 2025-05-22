27ee4cea1a689f4d373e408f3c418d4e07cc93cb
rhuanpk <dev@rhuanpk.net>
2025-05-22 13:43:43 -0300

    style: update variable name

--- prog/languages/golang/vanilla/scripts/sync_tl.go
+++ prog/languages/golang/vanilla/scripts/sync_tl.go
@@ -10,20 +10,20 @@ const delay = time.Millisecond * 500
 // go playground: https://go.dev/play/p/pMIuiUtnpFg
 func main() {
 	var wg sync.WaitGroup
-	tf := make(chan byte, 2) // up max to 2
+	tl := make(chan byte, 2) // up max to 2
 	for index := range 10 {
 		wg.Add(1)
-		tf <- 0
+		tl <- 0
 		go func(index int) {
 			defer func() {
 				wg.Done()
-				<-tf
+				<-tl
 			}()
 			println("index:", index)
 			time.Sleep(delay)
 		}(index)
 	}
 	wg.Wait()
-	close(tf)
+	close(tl)
 	println("finish")
 }
