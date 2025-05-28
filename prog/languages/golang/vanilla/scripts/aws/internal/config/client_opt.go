95f8c1b998c08b9ac05d25451d57a3c84135a034
rhuanpk <dev@rhuanpk.net>
2025-05-28 17:02:42 -0300

    feat: add a new way to get aws configs

---
+++ prog/languages/golang/vanilla/scripts/aws/internal/config/client_opt.go
@@ -0,0 +1,62 @@
+package config
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+)
+
+// Opt type fo handle with aws optFns.
+type Opt func(*config.LoadOptions) error
+
+// Creds struct to better handle with creds as params.
+type Creds struct {
+	AccessKey    string
+	SecretKey    string
+	SessionToken string
+}
+
+func loadCfg(ctx context.Context, opts ...Opt) (aws.Config, error) {
+	var optFns []func(*config.LoadOptions) error
+
+	for _, opt := range opts {
+		optFns = append(optFns, opt)
+	}
+
+	return config.LoadDefaultConfig(ctx, optFns...)
+}
+
+// NewOpts get a new slice of [Opt] to get a new config.
+func NewOpts(profile, region *string, creds *Creds) []Opt {
+	var opts []Opt
+
+	if profile != nil {
+		opts = append(opts, Opt(config.WithSharedConfigProfile(*profile)))
+	}
+
+	if region != nil {
+		opts = append(opts, Opt(config.WithRegion(*region)))
+	}
+
+	if creds != nil {
+		opts = append(opts, Opt(config.WithCredentialsProvider(
+			credentials.NewStaticCredentialsProvider(
+				creds.AccessKey, creds.SecretKey, creds.SessionToken,
+			),
+		)))
+	}
+
+	return opts
+}
+
+// Get get a new config with empty context.
+func Get(opts ...Opt) (aws.Config, error) {
+	return loadCfg(context.Background(), opts...)
+}
+
+// GetWithContext get a new config with epecified context.
+func GetWithContext(ctx context.Context, opts ...Opt) (aws.Config, error) {
+	return loadCfg(ctx, opts...)
+}
