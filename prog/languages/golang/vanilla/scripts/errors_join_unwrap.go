db54dc4f34698f0482b49122897db6929638e975
rhuanpk <dev@rhuanpk.net>
2025-06-09 16:34:18 -0300

    feat: add how to uwrap joinded erros go example

---
+++ prog/languages/golang/vanilla/scripts/errors_join_unwrap.go
@@ -0,0 +1,65 @@
+package main
+
+import "errors"
+
+type errOne struct {
+	message string
+}
+
+func (pr errOne) Error() string {
+	return pr.message
+}
+
+type errTwo struct {
+	message string
+}
+
+func (pr errTwo) Error() string {
+	return pr.message
+}
+
+func returnErrOne() error {
+	return errOne{"one"}
+}
+
+func returnErrTwo() error {
+	return errTwo{"two"}
+}
+
+func log(prefix string, err error) {
+	print(prefix + ": err != nil: ")
+	if err != nil {
+		println(true)
+	} else {
+		println(false)
+	}
+
+	print(prefix + ": errors.As(err, new(errOne)): ")
+	if errors.As(err, new(errOne)) {
+		println(true)
+	} else {
+		println(false)
+	}
+
+	print(prefix + ": errors.As(err, new(errTwo)): ")
+	if errors.As(err, new(errTwo)) {
+		println(true)
+	} else {
+		println(false)
+	}
+}
+
+func main() {
+	errOne := returnErrOne()
+	errTwo := returnErrTwo()
+
+	err := errors.Join(errOne, errTwo)
+	log("join", err)
+
+	// it could also be made recursive wrap if we split this block in a function
+	if ass, ok := err.(interface{ Unwrap() []error }); ok {
+		for _, err := range ass.Unwrap() {
+			log(err.Error(), err)
+		}
+	}
+}
